649b51e799e3cac27d9a456a16907aee985d95fa
Chen <ychen799@uwsp.edu>
2018-04-16 18:31:56 -0500

    Update authentication.php

--- authentication/authentication.php
+++ authentication/authentication.php
@@ -14,24 +14,20 @@ if (!$db->getConnStatus()) {
   exit;
 }
 
-$query = "select username, role.rolename, user.userpass, user.userstatus from user, user2role, role where user.userid = user2role.userid and user2role.roleid = role.roleid";
+$query = "select user.username, user.userpass, role.rolename from user, user2role, role where user.userid = user2role.userid and user2role.roleid = role.roleid and user.username = '{$_POST['usr']}';";
 $result = $db->dbCall($query);
 
 $exist = false;
+$role = array();
 
 #This part is for checking if the UserName that user typed exists.
 if(isset($_POST['usr']) && isset($_POST['passwd'])){
 	if (!empty($_POST['usr']) && !empty($_POST['passwd'])){
 		foreach($result as $self){
 			if($_POST['usr'] == $self['username']){
-				#$self['userstatus'] = 'Nonactive';
-				#print $self['userstatus'];
-				if ($self['userstatus'] == 'Active'){
-					$exist = true;
-					$role = $self['rolename'];
-					$hash_code = password_hash($self['userpass'],PASSWORD_DEFAULT); #we might not need this line of code.
-				}
-				break;
+				$exist = true;
+				$role[] += $self['rolename'];
+				$password = $self['userpass'];
 			}
 		}
     }elseif(empty($_POST['usr'])){
@@ -43,20 +39,16 @@ if(isset($_POST['usr']) && isset($_POST['passwd'])){
 #This part should check the UserName's password since UserName exists.
 if (isset($exist)){
 	if ($exist == true){
-		#var_dump($hash_code);
-		#var_dump(password_verify($_POST['passwd'], $hash_code));
-		if (password_verify($_POST['passwd'], $hash_code)){
+		if (password_verify($_POST['passwd'], $password)){
 			$_SESSION['usrname'] = $_POST['usr'];
 			$_SESSION['role'] = $role;
 			$_SESSION['login'] = true;
 			if(isset($_SESSION['usrname']) && isset($_SESSION['role'])){
-				print "Welcome, " . $_SESSION['usrname'] . ", you loged in as " . $_SESSION['role'] . ".";
+				header("Location: ../HomePage/HomePage.php");
 			}
 		}else{
 			print "Wrong Username or Password.";
 		}
-	}elseif ($exist == false && !empty($_POST['usr']) && !empty($_POST['passwd'])){
-		print "The account does not exist.";
 	}
 }
 
