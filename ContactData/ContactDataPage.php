1aae1c65159fe136771f9df8100607f32e821f89
Landrath <sland477@uwsp.edu>
2018-04-18 17:18:15 -0500

    Fixed Contact Data Page

--- ContactData/ContactDataPage.php
+++ ContactData/ContactDataPage.php
@@ -16,10 +16,8 @@ $page2->setFootSection();
 print $page->getTopSection();
 print $page2->getNavSection();
 
-$query = "select username, userpass, email, creationdate, realname, userstatus, rolename
-		from user, user2role, role
-		where user.userid = user2role.userid
-		and user2role.roleid = role.roleid";
+$query = "SELECT * FROM landrath_s.contactdata";
+
 $results = $db->dbCall($query);
 	
 if (!$db->getConnStatus()){
@@ -34,12 +32,9 @@ if(isset($_SESSION['usrname']) && in_array('admin', $_SESSION['role'], FALSE)){
 	<table>
 	<tr>
 		<th>ID</th>
-		<th>User Name</th>
 		<th>Email</th>
-		<th>Created On</th>
-		<th>Real Name</th>
-		<th>User Status</th>
-		<th>User Role</th>
+		<th>Phone Number</th>
+		<th>Additional Comments</th>
 	</tr>
 		
 	 <?php
