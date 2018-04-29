4f7899a690f2d6c483df36bc12a9d346fbae3b7c
Zogir <Zogir@users.noreply.github.com>
2018-04-29 16:50:44 -0500

    fixed inconsistent indentation in "REST_Service.php"

--- authentication/REST_Service.php
+++ authentication/REST_Service.php
@@ -5,8 +5,8 @@ require_once("../DB/DB.class.php");
 if(isset($_POST['data'])){
 	$db = new DB();
 	$data = $_POST['data']; #Get data from json
-    $obj = json_decode($data, true); #Decode to be array from data.
-    $safe_usr = $db->dbEsc($obj['usrname']);
+	$obj = json_decode($data, true); #Decode to be array from data.
+	$safe_usr = $db->dbEsc($obj['usrname']);
 	$query = "select user.username, user.userpass, user.realname, role.rolename from user, user2role, role where user.userid = user2role.userid and user2role.roleid = role.roleid and user.username = '{$safe_usr}';";
 }else{
     print json_encode("No-data"); #indicate there is no data from _POST.
@@ -17,14 +17,14 @@ if (!$db->getConnStatus()) {
 	print json_encode("DB-error"); #indicate not connected with database.
 	exit;
 }else{
-    $result = $db->dbCall($query);
+	$result = $db->dbCall($query);
 }
 
 #Check if the username exsits on database.
 if (!empty($result)){
-    print json_encode($result);
+	print json_encode($result);
 }else{
-    print json_encode("Null"); #Once query returns NULL(it means no such username), change to String "Null" and return to client.
+	print json_encode("Null"); #Once query returns NULL(it means no such username), change to String "Null" and return to client.
 }
 
 ?>
